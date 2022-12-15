bd5aa05a3842c761889aea81e266a31a5eef53b9
eocampo2021 <114036603+eocampo2021@users.noreply.github.com>
2022-12-15 19:09:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,35 @@ class ProductManager {
     products;
     constructor() {
       this.products = [];
+      this.product = Product;
     }
-    addProduct() {
 
+class Product extends ProductManager{
+  constructor (tittle, description, price, thumbnail, code, stock){
+  super()  
+  this.title = title;
+  this.description = description;
+  this.price = description;
+  this.thumbnail = thumbnail;
+  this.code = code;
+  this.stock = stock;
+  }
+}
+
+    addProduct(product) {
+        if(!product) return false;
+        if (this.alReadyExist(product)) throw
+        this.product.push (product);
     }
+     alReadyExist(product){
+        return (this.getProducts().find( actual => product.code == actual.code) !== undefined )
+     } 
+
     getProducts() {
-     
+     return this.products;
     }
+
     getProductById() {
-    
+     let product = this.getProducts().find(actual => id == actual.id);
     }
   }
